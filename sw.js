4c44a4e3b1b8b8da36384116478d59e8a50fb401
Ghadeer Elsharkawy <ghadeer.sharkawy@gmail.com>
2019-02-26 00:40:16 +0200

    fix: a bug fix fix code fetch in sw.js

--- sw.js
+++ sw.js
@@ -15,11 +15,15 @@ self.addEventListener('install',function (event) {
     );
 });
 
-self.addEventListener('fetch',function (event) {
+self.addEventListener('fetch', function(event) {
     event.respondWith(
-        caches.match(event.request).then(function (response) {
-            if(response) return response;
-            return fetch(response.request);
-        })
+      caches.match(event.request)
+        .then(function(response) {
+          if (response) {
+            return response;
+          }
+          return fetch(event.request);
+        }
+      )
     );
-});
+  });
