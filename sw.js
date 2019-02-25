9002898abfcb0651769d1cfc9cc8be1ca4552c41
Ghadeer Elsharkawy <ghadeer.sharkawy@gmail.com>
2019-02-26 00:29:40 +0200

    Declaring cache and cache files

---
+++ sw.js
@@ -0,0 +1,25 @@
+self.addEventListener('install',function (event) {
+    event.waitUntil(
+        caches.open('restaurant-review')
+        .then(function(cache) {
+            return cache.addAll([
+                'index.html',
+                'restaurant.html',
+                'css/styles.css',
+                'js/main.js',
+                'js/restaurant_info.js',
+                'js/dbhelper.js'
+                
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch',function (event) {
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            if(response) return response;
+            return fetch(response.request);
+        })
+    );
+});
