c9d0e1f246578fdc882e87a2ed1e0bf250ba8282
Ghadeer Elsharkawy <ghadeer.sharkawy@gmail.com>
2019-03-03 00:32:26 +0200

    fix Review project and fix code

--- sw.js
+++ sw.js
@@ -1,16 +1,25 @@
 self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open('review_app')
-        .then((caches) => {
-            return caches.addAll([
+        .then((cache) => {
+            return cache.addAll([
                 '/index.html',
                 '/restaurant.html',
                 '/js/dbhelper.js',
                 '/js/main.js',
                 '/js/restaurant_info.js',
-                '/js/sw_register.js',
                 '/data/restaurants.json',
-                '/css/styles.css'
+                '/css/styles.css',
+                '/img/1.jpg',
+                '/img/2.jpg',
+                '/img/3.jpg',
+                '/img/4.jpg',
+                '/img/5.jpg',
+                '/img/6.jpg',
+                '/img/7.jpg',
+                '/img/8.jpg',
+                '/img/9.jpg',
+                '/img/10.jpg',
             ]);
         })
     );
@@ -50,7 +59,7 @@ self.addEventListener('activate', (event)=> {
 
                     var responseToCache = response.clone();
 
-                    caches.open(review_app)
+                    caches.open('review_app')
                         .then((cache) => {
                             cache.put(event.request, responseToCache);
                         });
